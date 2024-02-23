49f476f957c79a5a53aab04abe141d68068c68d0
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-23 16:10:15 +0100

    add: layout projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 use App\Models\Project;
 
+
 class ProjectController extends Controller
 {
     public function index() {
 
         $projects = Project :: all();
+
 
         return view('pages.projects.index', compact('projects'));
     }
--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -3,13 +3,22 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>My projects</h1>
+    <h1>Projects</h1>
 
-    <ul>
-        @foreach ($types as $type)
-            <li>
-                <div><b>Type: </b> {{ $type->languages }}</div>
+    <ul class="d-flex justify-content-center flex-wrap">
+        @foreach ($projects as $project)
+            <li class="card text-start p-4 m-2" style="width: 500px">
+                <div><b>Title: </b>{{ $project->title }}</div>
+                <div><b>Description: </b>{{ $project->description }}</div>
+                <div><b>Type: </b>{{ $project->type->languages }}</div>
+                @foreach ($project->technologies as $technology)
+                    <div><b>Technologies: </b>{{ $technology->name }}</div>
+                @endforeach
+
+
             </li>
         @endforeach
+
+
     </ul>
 @endsection
